e7c09e8920a46e9c69821eb2c9b31226b64a7422
Amith Murali <amith0503@yahoo.com>
2021-05-08 17:14:15 +0100

    pushing again

---
+++ Non_Static_Methods_without_returntype/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Non_Static_Methods_without_returntype
+{
+    public class MainClass
+    {
+        private int i = 10;
+        private string s = "Amith";
+
+        public void function()
+        {
+            Console.WriteLine(i);
+        }
+        public void function1()
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Non_Static_Methods_without_returntype/demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Non_Static_Methods_without_returntype
+{
+    public class demo
+    {
+            public void Main(string[] args)
+            {
+                MainClass m = new MainClass();
+                m.function();
+                m.function1();
+            }
+        }
+    }
